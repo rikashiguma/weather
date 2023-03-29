333ddc94b8f4d4ef2ec0d69bb0da21de853c7f4f
Laura <laura.aliyarova@bk.ru>
2023-03-29 18:13:54 +0600

    fetchWeather

---
+++ script.js
@@ -0,0 +1,20 @@
+let weather = {
+    apiKey: "3c9f972ce73a0feb7ca40365114888b2",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    }
+}
+  
